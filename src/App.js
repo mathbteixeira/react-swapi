a8e6787716a9bc8dafe3aa30fce909542ed6d224
Matheus Batista Teixeira <mathbteixeira@gmail.com>
2020-09-13 23:06:09 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 import Home from './screens/home';
 import Detail from './screens/detail';
+import Film from './screens/film';
+import logo from './img/sw_logo.png';
+
+const menu = {
+    'color': 'yellow',
+    'background': 'black',
+    'text-decoration': 'none',
+    'fontFamily': `'Pathway Gothic One', sans-serif`
+};
+
+const img_responsive = {
+    'max-width': '100%',
+    'height': 'auto'
+};
+
+const no_bullets = {
+    'list-style-type': 'none'
+};
 
 const App = () => {
     return [
         <BrowserRouter>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/detail">Detail</Link></li>
+            <ul style={no_bullets}>
+                <li>
+                    <Link to="/" style={menu} >
+                        <img style={img_responsive} src={logo} alt="logo"/>
+                    </Link>
+                </li>
             </ul>
-            <Switch>    
+            <Switch>
+                <Route path='/films/:id'> <Film/> </Route>
                 <Route path='/detail/:id'> <Detail/> </Route>
                 <Route path='/'> <Home/> </Route>
             </Switch>
