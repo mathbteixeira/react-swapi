55b36044d244cfd72d0e2124fc27f9f0bc63c1cf
Matheus Batista Teixeira <mathbteixeira@gmail.com>
2020-09-13 23:06:39 -0300

    Add files via upload

--- src/helpers/ApiHelper.js
+++ src/helpers/ApiHelper.js
@@ -9,3 +9,8 @@ export const getIdFromUrl = (url) => {
     const matches = url.match(/\/([0-9]{1,})\//);
     return matches && matches[1] ? matches[1] : null;
 }
+
+export const convertDate = (date) => {
+    const matches = date.match(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/);
+    return matches && matches[1] ? matches[1] : null;
+}
